f09882e7429787c5851bb73e116f7f87fd5b83db
Jack <skelton.jt9@gmail.com>
2022-06-25 13:32:59 -0400

    fixed merge sort

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Navbar from "./Navbar";
 import "bootswatch/dist/darkly/bootstrap.min.css";
-import BubbleSort from "./BubbleSort";
+import BubbleSortDisplay from "./BubbleSort";
 import RandomArr from "./RandomArr";
-import QuickSort from "./QuickSort";
-import MergeSort from "./MergeSort";
+import QuickSortDisplay from "./QuickSort";
+import MergeSortDisplay from "./MergeSort";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
       <Header />
-      <BubbleSort />
-      <QuickSort />
-      <MergeSort />
+      <BubbleSortDisplay />
+      <QuickSortDisplay />
+      <MergeSortDisplay />
     </React.Fragment>
   );
 }
