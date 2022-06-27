87c5ddc8ac1adaba188ca067ce37c8ca36b5eee6
Jack <skelton.jt9@gmail.com>
2022-06-26 21:03:42 -0400

    wokring on styling

--- src/components/Body.js
+++ src/components/Body.js
@@ -102,17 +102,17 @@ function Body() {
           <pre>
             <code className="javascript-html">
               {`
-              function bubbleSort(arr) {
-                arr.map((e1) =>
-                  arr.map((e2, i) => {
-                    if (arr[i] > arr[i + 1]) {
-                      arr[i] = arr[i + 1];
-                      arr[i + 1] = e2;
-                    }
-                  })
-                );
-                return arr;
-              }
+  function bubbleSort(arr) {
+    arr.map((e1) =>
+      arr.map((e2, i) => {
+        if (arr[i] > arr[i + 1]) {
+          arr[i] = arr[i + 1];
+          arr[i + 1] = e2;
+        }
+      })
+    );
+    return arr;
+  }
               `}
             </code>
           </pre>
@@ -121,23 +121,23 @@ function Body() {
           <pre>
             <code className="javascript-html">
               {`
-              function quickSort(origArray) {
-                if (origArray.length <= 1) return origArray;
-              
-                var leftArray = [];
-                var rightArray = [];
-                var pivot = origArray[origArray.length - 1];
-              
-                for (var i = 0; i < origArray.length - 1; i++) {
-                  if (origArray[i] < pivot) {
-                    leftArray.push(origArray[i]);
-                  } else {
-                    rightArray.push(origArray[i]);
-                  }
-                }
-                var newArray = [...quickSort(leftArray), pivot, ...quickSort(rightArray)];
-                return newArray;
-              }
+  function quickSort(origArray) {
+    if (origArray.length <= 1) return origArray;
+  
+    var leftArray = [];
+    var rightArray = [];
+    var pivot = origArray[origArray.length - 1];
+  
+    for (var i = 0; i < origArray.length - 1; i++) {
+      if (origArray[i] < pivot) {
+        leftArray.push(origArray[i]);
+      } else {
+        rightArray.push(origArray[i]);
+      }
+    }
+    var newArray = [...quickSort(leftArray), pivot, ...quickSort(rightArray)];
+    return newArray;
+  }
               `}
             </code>
           </pre>
@@ -146,34 +146,34 @@ function Body() {
           <pre>
             <code className="javascript-html">
               {`
-             const mergeSort = (array) => {
-              if (array.length <= 1) {
-                return array;
-              }
-              const middleIndex = Math.floor(array.length / 2);
-              const leftArray = array.slice(0, middleIndex);
-              const rightArray = array.slice(middleIndex);
-            
-              return merge(mergeSort(leftArray), mergeSort(rightArray));
-            };
-            
-            const merge = (leftArr, rightArr) => {
-              const output = [];
-              let leftIndex = 0;
-              let rightIndex = 0;
-            
-              while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
-                const leftElement = leftArr[leftIndex];
-                const rightElement = rightArr[rightIndex];
-            
-                if (leftElement < rightElement) {
-                  output.push(leftElement);
-                  leftIndex++;
-                } else {
-                  output.push(rightElement);
-                  rightIndex++;
-                }
-              }
+  const mergeSort = (array) => {
+    if (array.length <= 1) {
+      return array;
+    }
+    const middleIndex = Math.floor(array.length / 2);
+    const leftArray = array.slice(0, middleIndex);
+    const rightArray = array.slice(middleIndex);
+  
+    return merge(mergeSort(leftArray), mergeSort(rightArray));
+  };
+  
+  const merge = (leftArr, rightArr) => {
+    const output = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+  
+    while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+      const leftElement = leftArr[leftIndex];
+      const rightElement = rightArr[rightIndex];
+  
+      if (leftElement < rightElement) {
+        output.push(leftElement);
+        leftIndex++;
+      } else {
+        output.push(rightElement);
+        rightIndex++;
+      }
+    }
               `}
             </code>
           </pre>
@@ -182,18 +182,18 @@ function Body() {
           <pre>
             <code className="javascript-html">
               {`
-              function InsertionSort(arr) {
-                for (let i = 1; i < arr.length; i++) {
-                  for (let j = i; j > 0; j--) {
-                    if (arr[j] < arr[j - 1]) {
-                      const tempValue = arr[j];
-                      arr[j] = arr[j - 1];
-                      arr[j - 1] = tempValue;
-                    } else break;
-                  }
-                }
-                return arr;
-              }
+  function InsertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      for (let j = i; j > 0; j--) {
+        if (arr[j] < arr[j - 1]) {
+          const tempValue = arr[j];
+          arr[j] = arr[j - 1];
+          arr[j - 1] = tempValue;
+        } else break;
+      }
+    }
+    return arr;
+  }
               `}
             </code>
           </pre>
@@ -202,21 +202,21 @@ function Body() {
           <pre>
             <code className="javascript-html">
               {`
-              function SelectionSort(array) {
-                const arr = array.map((x) => {
-                  return x;
-                });
-                for (let i = 0; i < arr.length - 1; i++) {
-                  let minIndex = i;
-                  for (let j = i + 1; j < arr.length; j++) {
-                    if (arr[j] < arr[minIndex]) {
-                      minIndex = j;
-                    }
-                  }
-                  [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-                }
-                return arr;
-              }
+  function SelectionSort(array) {
+    const arr = array.map((x) => {
+      return x;
+    });
+    for (let i = 0; i < arr.length - 1; i++) {
+      let minIndex = i;
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[minIndex]) {
+          minIndex = j;
+        }
+      }
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+    return arr;
+  }
               `}
             </code>
           </pre>
@@ -225,35 +225,35 @@ function Body() {
           <pre>
             <code className="javascript-html">
               {`
-              function HeapSort(arr) {
-                for (var i = Math.floor(arr.length / 2) - 1; i >= 0; i--)
-                  heapify(arr, arr.length, i);
-              
-                for (var j = arr.length - 1; j > 0; j--) {
-                  var temp = arr[0];
-                  arr[0] = arr[j];
-                  arr[j] = temp;
-                  heapify(arr, j, 0);
-                }
-                return arr;
-              }
-              
-              function heapify(arr, arrLength, i) {
-                var largest = i;
-                var left = 2 * i + 1;
-                var right = 2 * i + 2;
-              
-                if (left < arrLength && arr[left] > arr[largest]) largest = left;
-              
-                if (right < arrLength && arr[right] > arr[largest]) largest = right;
-              
-                if (largest !== i) {
-                  [arr[i], arr[largest]] = [arr[largest], arr[i]];
-              
-                  heapify(arr, arrLength, largest);
-                }
-                return arr;
-              }
+  function HeapSort(arr) {
+    for (var i = Math.floor(arr.length / 2) - 1; i >= 0; i--)
+      heapify(arr, arr.length, i);
+  
+    for (var j = arr.length - 1; j > 0; j--) {
+      var temp = arr[0];
+      arr[0] = arr[j];
+      arr[j] = temp;
+      heapify(arr, j, 0);
+    }
+    return arr;
+  }
+  
+  function heapify(arr, arrLength, i) {
+    var largest = i;
+    var left = 2 * i + 1;
+    var right = 2 * i + 2;
+  
+    if (left < arrLength && arr[left] > arr[largest]) largest = left;
+  
+    if (right < arrLength && arr[right] > arr[largest]) largest = right;
+  
+    if (largest !== i) {
+      [arr[i], arr[largest]] = [arr[largest], arr[i]];
+  
+      heapify(arr, arrLength, largest);
+    }
+    return arr;
+  }
               `}
             </code>
           </pre>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 function Header() {
   return (
     <React.Fragment>
-      <h1>Learn Sort</h1>
+      <div className="header">
+        <h1>Learn Sort</h1>
+      </div>
     </React.Fragment>
   );
 }
