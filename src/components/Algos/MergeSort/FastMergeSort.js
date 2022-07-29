fbc8158294605188ac31d964d6153b09212a92d6
Jack <skelton.jt9@gmail.com>
2022-07-29 17:15:15 -0400

    trying to add animations to merge

--- src/components/Algos/MergeSort/FastMergeSort.js
+++ src/components/Algos/MergeSort/FastMergeSort.js
@@ -54,71 +54,143 @@ function MergeSortDisplayFast() {
   }
 
   const drawSorted = async (context) => {
-    const arr = [...arrValues];
-    const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-    for (i; i < arr.length - 1; i++) {
-      for (j; j < arr.length - 1 - i; j++) {
-        if (buttonTextRef.current === "Start") return;
-        if (arr[j] > arr[j + 1]) {
-          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-          context.clearRect((j + 1) * 10, 0, 8, 600);
-          context.fillStyle = "#00bc8c";
-          context.fillRect(
-            (j + 1) * 10,
-            600 - 50 * arr[j + 1],
-            8,
-            50 * arr[j + 1]
-          );
-          context.clearRect(j * 10, 0, 8, 600);
-          context.fillStyle = "#f39c12";
-          context.fillRect(j * 10, 600 - 50 * arr[j], 8, 50 * arr[j]);
+    const array = [...arrValues];
 
-          context.clearRect((j - 1) * 10, 0, 8, 600);
-          context.fillStyle = "#adb5bd";
-          context.fillRect(
-            (j - 1) * 10,
-            600 - 50 * arr[j - 1],
-            8,
-            50 * arr[j - 1]
-          );
-        } else if (arr[j] <= arr[j + 1]) {
-          context.clearRect((j + 1) * 10, 0, 8, 600);
-          context.fillStyle = "#00bc8c";
-          context.fillRect(
-            (j + 1) * 10,
-            600 - 50 * arr[j + 1],
-            8,
-            50 * arr[j + 1]
-          );
-
-          context.clearRect((j - 1) * 10, 0, 8, 600);
-          context.fillStyle = "#adb5bd";
-          context.fillRect(
-            (j - 1) * 10,
-            600 - 50 * arr[j - 1],
-            8,
-            50 * arr[j - 1]
-          );
-
-          context.clearRect(j * 10, 0, 8, 600);
-          context.fillStyle = "#f39c12";
-          context.fillRect(j * 10, 600 - 50 * arr[j], 8, 50 * arr[j]);
-        }
-        if (j === arr.length - 2 - i) {
-          context.clearRect(j * 10, 0, 8, 600);
-          context.fillStyle = "#adb5bd";
-          context.fillRect(j * 10, 600 - 50 * arr[j], 8, 50 * arr[j]);
-        }
-        if (arr.length - 1 - i === 1) {
-          context.clearRect(j * 10, 0, 8, 600);
-          context.fillStyle = "#00bc8c";
-          context.fillRect(j * 10, 600 - 50 * arr[j], 8, 50 * arr[j]);
-        }
-        await wait(10);
+    const mergeSort = (array) => {
+      if (array.length <= 1) {
+        return array;
       }
-      j = 0;
-    }
+      const middleIndex = Math.floor(array.length / 2);
+      console.log("MI " + middleIndex);
+      const leftArray = array.slice(0, middleIndex);
+      console.log("LA " + leftArray);
+      const rightArray = array.slice(middleIndex);
+      console.log("RA " + rightArray);
+      return merge(mergeSort(leftArray), mergeSort(rightArray));
+    };
+
+    const merge = async (leftArr, rightArr) => {
+      const output = [];
+      let leftIndex = 0;
+      let rightIndex = 0;
+
+      while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+        const i = leftArr[leftIndex];
+        console.log("LE " + i);
+        const j = rightArr[rightIndex];
+        console.log("RE " + j);
+
+        if (i < j) {
+          output.push(i);
+          leftIndex++;
+        } else {
+          context.clearRect((j + 1) * 60, 0, 58, 300);
+          context.fillStyle = "#e74c3c";
+          context.fillRect(
+            (j + 1) * 60,
+            300 - 30 * array[j + 1],
+            58,
+            30 * array[j + 1]
+          );
+
+          context.clearRect(j * 60, 0, 58, 300);
+          context.fillStyle = "#e74c3c";
+          context.fillRect(j * 60, 300 - 30 * array[j], 58, 30 * array[j]);
+
+          context.clearRect((j - 1) * 60, 0, 58, 300);
+          context.fillStyle = "#adb5bd";
+          context.fillRect(
+            (j - 1) * 60,
+            300 - 30 * array[j - 1],
+            58,
+            30 * array[j - 1]
+          );
+
+          await wait(300);
+
+          output.push(j);
+          rightIndex++;
+        }
+      }
+      console.log("OP " + output);
+      console.log([
+        ...output,
+
+        ...leftArr.slice(leftIndex),
+
+        ...rightArr.slice(rightIndex),
+      ]);
+      return [
+        ...output,
+        ...leftArr.slice(leftIndex),
+        ...rightArr.slice(rightIndex),
+      ];
+    };
+    mergeSort(array);
   };
+
+  // for (i; i < arr.length - 1; i++) {
+  //   for (j; j < arr.length - 1 - i; j++) {
+  //     if (buttonTextRef.current === "Start") return;
+  //     if (arr[j] > arr[j + 1]) {
+  //       [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+  //       context.clearRect((j + 1) * 10, 0, 8, 600);
+  //       context.fillStyle = "#00bc8c";
+  //       context.fillRect(
+  //         (j + 1) * 10,
+  //         600 - 50 * arr[j + 1],
+  //         8,
+  //         50 * arr[j + 1]
+  //       );
+  //       context.clearRect(j * 10, 0, 8, 600);
+  //       context.fillStyle = "#f39c12";
+  //       context.fillRect(j * 10, 600 - 50 * arr[j], 8, 50 * arr[j]);
+
+  //       context.clearRect((j - 1) * 10, 0, 8, 600);
+  //       context.fillStyle = "#adb5bd";
+  //       context.fillRect(
+  //         (j - 1) * 10,
+  //         600 - 50 * arr[j - 1],
+  //         8,
+  //         50 * arr[j - 1]
+  //       );
+  //     } else if (arr[j] <= arr[j + 1]) {
+  //       context.clearRect((j + 1) * 10, 0, 8, 600);
+  //       context.fillStyle = "#00bc8c";
+  //       context.fillRect(
+  //         (j + 1) * 10,
+  //         600 - 50 * arr[j + 1],
+  //         8,
+  //         50 * arr[j + 1]
+  //       );
+
+  //       context.clearRect((j - 1) * 10, 0, 8, 600);
+  //       context.fillStyle = "#adb5bd";
+  //       context.fillRect(
+  //         (j - 1) * 10,
+  //         600 - 50 * arr[j - 1],
+  //         8,
+  //         50 * arr[j - 1]
+  //       );
+
+  //       context.clearRect(j * 10, 0, 8, 600);
+  //       context.fillStyle = "#f39c12";
+  //       context.fillRect(j * 10, 600 - 50 * arr[j], 8, 50 * arr[j]);
+  //     }
+  //     if (j === arr.length - 2 - i) {
+  //       context.clearRect(j * 10, 0, 8, 600);
+  //       context.fillStyle = "#adb5bd";
+  //       context.fillRect(j * 10, 600 - 50 * arr[j], 8, 50 * arr[j]);
+  //     }
+  //     if (arr.length - 1 - i === 1) {
+  //       context.clearRect(j * 10, 0, 8, 600);
+  //       context.fillStyle = "#00bc8c";
+  //       context.fillRect(j * 10, 600 - 50 * arr[j], 8, 50 * arr[j]);
+  //     }
+  //     await wait(10);
+  //   }
+  //   j = 0;
+  // }
 
   return (
     <div className="mergeSortFast">
--- src/components/Algos/MergeSort/MergeSort.js
+++ src/components/Algos/MergeSort/MergeSort.js
@@ -1,12 +1,17 @@
 import RandomArr from "../FunctionsForAll/RandomArr";
 import React, { useState } from "react";
+import Canvas from "../FunctionsForAll/Canvas";
+import Draw from "../FunctionsForAll/DrawArray";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 const mergeSort = (array) => {
   if (array.length <= 1) {
     return array;
   }
   const middleIndex = Math.floor(array.length / 2);
+
   const leftArray = array.slice(0, middleIndex);
+
   const rightArray = array.slice(middleIndex);
 
   return merge(mergeSort(leftArray), mergeSort(rightArray));
@@ -19,6 +24,7 @@ const merge = (leftArr, rightArr) => {
 
   while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
     const leftElement = leftArr[leftIndex];
+
     const rightElement = rightArr[rightIndex];
 
     if (leftElement < rightElement) {
@@ -30,30 +36,173 @@ const merge = (leftArr, rightArr) => {
     }
   }
 
-  return [...output, leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)];
+  return [
+    ...output,
+    ...leftArr.slice(leftIndex),
+    ...rightArr.slice(rightIndex),
+  ];
 };
 
+let waitForPressResolve = true;
+function waitForPress() {
+  return new Promise((resolve) => (waitForPressResolve = resolve));
+}
+
+function btnResolver() {
+  if (waitForPressResolve) waitForPressResolve();
+}
+
 function MergeSortDisplay() {
-  const [arrValues, setArrValues] = useState(() => {
-    return RandomArr();
-  });
-  const [sortedArrValues, setSortedArrValues] = useState(() => {
-    return "";
-  });
+  const [arrValues, setArrValues] = useState(RandomArr(10));
+  const [sortedArrValues, setSorted] = useState([]);
+  const [canvas, setCanvas] = useState(
+    <Canvas array={[...arrValues]} draw={Draw} height={300} width={600} />
+  );
 
   function sortThatArray() {
-    setSortedArrValues(mergeSort(arrValues));
+    const array = [...arrValues];
+    setSorted(mergeSort(array));
+    setCanvas(<Canvas draw={drawSorted} height={300} width={600} />);
   }
 
-  function newArray() {
-    setArrValues(RandomArr());
+  function NewArray() {
+    const array = RandomArr(10);
+    setArrValues(array);
+    setSorted(" ");
+    setCanvas(
+      <Canvas array={[...array]} draw={Draw} height={300} width={600} />
+    );
   }
+
+  const drawSorted = async (context) => {
+    const array = [...arrValues];
+
+    const mergeSort = async (array) => {
+      if (array.length <= 1) {
+        return array;
+      }
+      const middleIndex = Math.floor(array.length / 2);
+      console.log("MI " + middleIndex);
+      const leftArray = array.slice(0, middleIndex);
+      console.log("LA " + leftArray);
+      const rightArray = array.slice(middleIndex);
+      console.log("RA " + rightArray);
+      console.log("WAIT");
+      await waitForPress();
+      return merge(
+        await mergeSort(leftArray),
+        await mergeSort(rightArray),
+        middleIndex
+      );
+    };
+
+    const merge = async (leftArr, rightArr, midIndx) => {
+      const output = [];
+      let leftIndex = 0;
+      let rightIndex = 0;
+
+      while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+        const leftElement = leftArr[leftIndex];
+        console.log("LE " + leftElement);
+        const rightElement = rightArr[rightIndex];
+        console.log("RE " + rightElement);
+
+        context.clearRect(array[leftIndex] * 60, 0, 58, 300);
+        context.fillStyle = "#3498db";
+        context.fillRect(
+          leftIndex * 60,
+          300 - 30 * leftElement,
+          58,
+          30 * leftElement
+        );
+
+        context.clearRect(array[leftIndex] * 60, 0, 58, 300);
+        context.fillStyle = "#3498db";
+        context.fillRect(
+          leftIndex * 60,
+          300 - 30 * leftElement,
+          58,
+          30 * leftElement
+        );
+
+        await waitForPress();
+
+        if (leftElement <= rightElement) {
+          output.push(leftElement);
+          console.log("Output " + output);
+          leftIndex++;
+        } else {
+          //fill with red
+          context.clearRect(leftIndex * 60, 0, 58, 300);
+          context.fillStyle = "#e74c3c";
+          context.fillRect(
+            leftIndex * 60,
+            300 - 30 * leftElement,
+            58,
+            30 * leftElement
+          );
+
+          context.clearRect(midIndx * 60, 0, 58, 300);
+          context.fillStyle = "#e74c3c";
+          context.fillRect(
+            midIndx * 60,
+            300 - 30 * rightElement,
+            58,
+            30 * rightElement
+          );
+
+          await wait(300);
+
+          //swap
+          context.clearRect(leftIndex * 60, 0, 58, 300);
+          context.fillStyle = "#00bc8c";
+          context.fillRect(
+            leftIndex * 60,
+            300 - 30 * rightElement,
+            58,
+            30 * rightElement
+          );
+
+          context.clearRect(midIndx * 60, 0, 58, 300);
+          context.fillStyle = "#f39c12";
+          context.fillRect(
+            midIndx * 60,
+            300 - 30 * leftElement,
+            58,
+            30 * leftElement
+          );
+
+          output.push(rightElement);
+          console.log("Output " + output);
+          rightIndex++;
+        }
+      }
+      console.log("LEFT FINAL " + leftArr.slice(leftIndex));
+      console.log("RIGHT FINAL " + rightArr.slice(rightIndex));
+      console.log(
+        "Returned Array " +
+          [
+            ...output,
+
+            ...leftArr.slice(leftIndex),
+
+            ...rightArr.slice(rightIndex),
+          ]
+      );
+      return [
+        ...output,
+        ...leftArr.slice(leftIndex),
+        ...rightArr.slice(rightIndex),
+      ];
+    };
+    mergeSort(array);
+  };
 
   return (
     <div className="MergeSort">
       <div className="card-header">Merge Sort</div>
       <div className="card-body">
-        <button type="button" className="btn btn-success" onClick={newArray}>
+        <button type="button" className="btn btn-success" onClick={NewArray}>
           Generate New Array
         </button>
         <br></br>
@@ -69,6 +218,15 @@ function MergeSortDisplay() {
         <span>Origional Array: {arrValues}</span>
         <br></br>
         <span>Merge Sorted Array: {sortedArrValues}</span>
+        <span>{canvas}</span>
+        <button
+          id="next"
+          type="button"
+          className="btn btn-success"
+          onClick={btnResolver}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
